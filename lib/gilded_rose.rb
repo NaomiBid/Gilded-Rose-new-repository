d73ea018a1aa8bf20d05fd0f664da3d8d23bf9be
NaomiBid <naomi0bid@gmail.com>
2019-01-13 23:07:21 +0000

    created method for updating a normal item

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,6 +6,9 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
+      if item.name == "item"
+        return update_normal
+      end
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
@@ -51,4 +54,16 @@ class GildedRose
       end
     end
   end
+
+  def update_normal
+    if @items[0].quality > 0
+      if @items[0].sell_in > 0
+        @items[0].quality = @items[0].quality - 1
+      else
+        @items[0].quality = @items[0].quality - 2
+      end
+    end
+    @items[0].sell_in = @items[0].sell_in - 1
+  end
+  
 end
