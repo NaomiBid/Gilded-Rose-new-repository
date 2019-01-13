d6931a7aaf2bbb3588ebc9f9724d3d4c1737de23
NaomiBid <naomi0bid@gmail.com>
2019-01-13 23:26:09 +0000

    created update_brie method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,6 +9,9 @@ class GildedRose
       if item.name == "item"
         return update_normal
       end
+      if item.name == "Aged Brie"
+        return update_brie
+      end
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
@@ -65,5 +68,20 @@ class GildedRose
     end
     @items[0].sell_in = @items[0].sell_in - 1
   end
-  
+
+  def update_brie
+    if @items[0].quality == 49
+      @items[0].quality = @items[0].quality + 1
+    end
+    if @items[0].quality == 50
+      return
+    end
+    if @items[0].sell_in > 0
+      @items[0].quality = @items[0].quality + 1
+    else
+      @items[0].quality = @items[0].quality + 2
+    end
+    @items[0].sell_in = @items[0].sell_in - 1
+  end
+
 end
