1469861b4aceb46c38e54ac71bbedce7d4c46916
NaomiBid <naomi0bid@gmail.com>
2019-01-14 00:11:43 +0000

    created methods for update_backstage passes and sulphuras, deleted redundant code

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -12,48 +12,11 @@ class GildedRose
       if item.name == "Aged Brie"
         return update_brie
       end
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
+      if item.name == "Sulfuras, Hand of Ragnaros"
+        return update_sulphuras
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+      if item.name == "Backstage passes to a TAFKAL80ETC concert"
+        return update_backstage
       end
     end
   end
@@ -82,6 +45,36 @@ class GildedRose
       @items[0].quality = @items[0].quality + 2
     end
     @items[0].sell_in = @items[0].sell_in - 1
+  end
+
+  def update_sulphuras
+  end
+
+  def update_backstage
+    if @items[0].sell_in > 10
+      if @items[0].quality < 50
+        @items[0].quality = @items[0].quality + 1
+      end
+
+    elsif @items[0].sell_in < 11 && @items[0].sell_in > 5
+      if @items[0].quality == 49
+        @items[0].quality = 50
+      else
+        @items[0].quality = @items[0].quality + 2
+      end
+
+    elsif @items[0].sell_in < 6 && @items[0].sell_in > 0
+      if @items[0].quality == 48
+        @items[0].quality = 50
+      else
+        @items[0].quality = @items[0].quality + 3
+      end
+    end
+
+    @items[0].sell_in = @items[0].sell_in - 1
+    if @items[0].sell_in <= 0
+      @items[0].quality = 0
+    end
   end
 
 end
