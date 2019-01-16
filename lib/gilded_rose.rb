3c12ee760c8e3726b533ec44b4fb4f16458e346a
NaomiBid <naomi0bid@gmail.com>
2019-01-16 14:32:27 +0000

    refactored update_backstage method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -38,27 +38,15 @@ class GildedRose
   end
 
   def update_backstage
-    if @items[0].sell_in > 10
-      if @items[0].quality < 50
-        @items[0].quality = @items[0].quality + 1
-      end
-    elsif @items[0].sell_in < 11 && @items[0].sell_in > 5
-      if @items[0].quality == 49
-        @items[0].quality = 50
-      else
-        @items[0].quality = @items[0].quality + 2
-      end
-    elsif @items[0].sell_in < 6 && @items[0].sell_in > 0
-      if @items[0].quality == 48
-        @items[0].quality = 50
-      else
-        @items[0].quality = @items[0].quality + 3
-      end
-    end
+    @items[0].quality = 0 if @items[0].sell_in <= 0
     @items[0].sell_in = @items[0].sell_in - 1
-    if @items[0].sell_in <= 0
-      @items[0].quality = 0
-    end
+    return if @items[0].quality >= 50
+    @items[0].quality = @items[0].quality + 1
+    return if @items[0].quality >= 50
+    @items[0].quality = @items[0].quality + 1 if @items[0].sell_in < 10
+    return if @items[0].quality >= 50
+    @items[0].quality = @items[0].quality + 1 if @items[0].sell_in < 5
+    @items[0].quality = 0 if @items[0].sell_in <= 0
   end
 
 end
