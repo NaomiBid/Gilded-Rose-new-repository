625bf485818a6a956d16c094f4d7bf6de27c754d
NaomiBid <naomi0bid@gmail.com>
2019-01-23 15:38:12 +0000

    got rid of case statement in update_quality method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -10,22 +10,12 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      case item.name
-      when "item"
-        item.update
-      when "Aged Brie"
-        item.update
-      when "Sulfuras, Hand of Ragnaros"
+      if item.name == "Sulfuras, Hand of Ragnaros"
         return item
-      when "Backstage passes to a TAFKAL80ETC concert"
+      else
         item.update
       end
     end
   end
-
-#  def update_backstage(item)
-#    item = Backstage.new(item)
-#    item.update
-#  end
 
 end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -5,7 +5,7 @@ describe GildedRose do
 
   describe "#update_quality" do
     it "does not change the name" do
-      item = Item.new("foo", 0, 0)
+      item = Normal.new("foo", 0, 0)
       GildedRose.new([item]).update_quality
       expect(item.name).to eq "foo"
     end
