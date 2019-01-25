4a6706e116e1f1b8317c8a6182b0a3a398ba3191
NaomiBid <naomi0bid@gmail.com>
2019-01-25 15:25:33 +0000

    created update_sell_in and update_quality methods in Backstage class

--- lib/backstage.rb
+++ lib/backstage.rb
@@ -3,14 +3,20 @@ require 'item'
 class Backstage < Item
 
   def update
-    @quality = 0 if @sell_in <= 0
+    update_sell_in
+    update_quality
+  end
+
+  def update_sell_in
     @sell_in -= 1
-    return if @quality >= 50
-    @quality += 1
-    return if @quality >= 50
+  end
+
+  def update_quality
+    @quality = 0 if @sell_in <= 0
+    @quality += 1 if @sell_in < 5
     @quality += 1 if @sell_in < 10
     return if @quality >= 50
-    @quality += 1 if @sell_in < 5
+    @quality += 1
     @quality = 0 if @sell_in <= 0
   end
 
