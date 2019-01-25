101b56b887f7878a5e738f1f65d490768c8206f6
NaomiBid <naomi0bid@gmail.com>
2019-01-25 15:03:22 +0000

    created update_sell_in and update_quality methods for Conjured class

--- lib/conjured.rb
+++ lib/conjured.rb
@@ -2,9 +2,18 @@ require 'item'
 
 class Conjured < Item
   def update
+    update_sell_in
+    update_quality
+  end
+
+  def update_sell_in
     @sell_in -= 1
+  end
+
+  def update_quality
     return if @quality = 0
     @quality -= 2
     @quality -= 2 if @sell_in <= 0
   end
+
 end
