f098ec799e2d19fb7c58d742e587d0e2d183ed61
NaomiBid <naomi0bid@gmail.com>
2019-01-25 14:59:45 +0000

    created update_sell_in and update_quality methods for Brie class to split responsibilities

--- lib/brie.rb
+++ lib/brie.rb
@@ -3,7 +3,15 @@ require 'item'
 class Brie < Item
 
   def update
+    update_sell_in
+    update_quality
+  end
+
+  def update_sell_in
     @sell_in -= 1
+  end
+
+  def update_quality
     return if @quality >= 50
     @quality += 1
     return if @quality >= 50
