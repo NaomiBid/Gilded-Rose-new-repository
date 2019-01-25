e5bac747205e645fd98242c0a73073cd6669327c
NaomiBid <naomi0bid@gmail.com>
2019-01-25 14:54:50 +0000

    created update_sell_in and update_quality methods in Normal class to split out responsibilities

--- lib/normal.rb
+++ lib/normal.rb
@@ -3,10 +3,18 @@ require 'item'
 class Normal < Item
 
   def update
-      @sell_in -= 1
-      return if @quality = 0
-      @quality -= 1
-      @quality -= 1 if @sell_in <= 0
+    update_sell_in
+    return if @quality = 0
+    update_quality
+  end
+
+  def update_sell_in
+    @sell_in -= 1
+  end
+
+  def update_quality
+    @quality -= 1
+    @quality -= 1 if @sell_in <= 0
   end
 
 end
